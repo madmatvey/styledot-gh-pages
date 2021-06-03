4cc65370e18025c3fb50d53aa25ebf02de5d355e
madmatvey <potehin@gmail.com>
2021-06-03 19:35:51 +0400

    0181dd5

---
+++ _nuxt/static/1622734543/services/wardrobe_capsule_online/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622734543",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices\u002Fwardrobe_capsule_online",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b0bcdf5"}}}}(null));
